7f049df392ac64cc7b5b63f5ab02125a49ffc82d
pc <nikitamilovatskiy@gmail.com>
2023-10-10 22:19:21 +0300

    feat: add hull to spaceship and jump engine to spaceship.travel

--- src/Lab1/Engines/Entities/JumpEngine.cs
+++ src/Lab1/Engines/Entities/JumpEngine.cs
@@ -5,5 +5,5 @@ namespace Itmo.ObjectOrientedProgramming.Lab1.Engines.Entities;
 public abstract class JumpEngine
 {
     public Fuel FuelType { get; init; } = Fuel.GravitonMatter;
-    public SubspaceTravel SubspaceTravelDistance { get; init; }
+    public int SubspaceTravelDistance { get; init; }
 }
