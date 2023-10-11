585dcdad6534633374946f3a6c00f522c7a51f4b
pc <nikitamilovatskiy@gmail.com>
2023-10-11 17:11:06 +0300

    fix: add parameterless constructor to Deflector.cs to make decorators work

--- src/Lab1/Deflectors/Entities/Deflector.cs
+++ src/Lab1/Deflectors/Entities/Deflector.cs
@@ -2,6 +2,10 @@ namespace Itmo.ObjectOrientedProgramming.Lab1.Deflectors.Entities;
 
 public abstract class Deflector : IDeflector
 {
+    protected Deflector()
+    {
+    }
+
     protected Deflector(int hp)
     {
         Hp = hp;
