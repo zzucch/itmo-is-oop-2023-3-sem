c527c61a3595b033909b6e961b464562879ca68e
pc <nikitamilovatskiy@gmail.com>
2023-10-13 23:47:47 +0300

    feat: remove interfaces unnecessary for business logic

--- src/Lab1/Entities/Engines/IEngine.cs
+++
@@ -1,8 +0,0 @@
-using Itmo.ObjectOrientedProgramming.Lab1.Models;
-
-namespace Itmo.ObjectOrientedProgramming.Lab1.Entities.Engines;
-
-public interface IEngine
-{
-    TravelResult TryTravel(int distanceLightYear, EnvironmentType environmentType);
-}
